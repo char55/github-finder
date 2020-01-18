d2cfc709953fcd190f42fdb5747905126ced04bf
charlotte <charas@dynamite.ca>
2020-01-18 11:26:38 -0500

    search made

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,11 +1,37 @@
-import React from 'react'
 
-function Search() {
-    return (
-        <div>
-            
+import React, { Component } from 'react'
+
+
+export default class Search extends Component {
+    state = {
+        text: ''
+    }
+
+    onChange = e => this.setState({[e.target.name] : e.target.value}) 
+    onSubmit = e => {
+        this.props.searchUsers(this.state.text)
+        e.preventDefault()
+        this.setState({text: ''})
+    }
+
+    render() {
+        return (
+            <div>
+            <form onSubmit={this.onSubmit} className="form">
+                <input 
+                    value={this.state.text} 
+                    onChange={this.onChange}
+                    type="text" 
+                    name="text" 
+                    placeholder="Search Users..."
+                />
+                <input 
+                    type="submit"
+                    value="Search"
+                    className="btn btn-dark btn-block"
+                />
+            </form>
         </div>
-    )
+        )
+    }
 }
-
-export default Search
