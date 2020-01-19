afdb3ed19697494dfaaae3a22f4f24c216e0f1bf
charlotte <charas@dynamite.ca>
2020-01-19 15:32:35 -0500

    deploy to netlify

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ import Users from './components/users/Users';
 import Search from './components/users/Search';
 import axios from 'axios';
 
+let githubClientId;
+let githubClientSecret;
+
+if (process.env.NODE_ENV != 'production'){
+  githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
+  githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
+} else {
+  githubClientId = process.env.GITHUB_CLIENT_ID;
+  githubClientSecret = process.env.GITHUB_CLIENT_SECRET;
+}
 class App extends Component {
   // render is a life cycle method that creates the output
   // whihc is included in Component
@@ -25,20 +35,20 @@ class App extends Component {
     this.setState({loading: true})
     const res = 
     searchText === '' ?
-    await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
-    : await axios.get(`https://api.github.com/search/users?q=${searchText}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+    await axios.get(`https://api.github.com/users?client_id=${githubClientId}&client_secret=${githubClientSecret}`)
+    : await axios.get(`https://api.github.com/search/users?q=${searchText}&client_id=${githubClientId}&client_secret=${githubClientSecret}`)
     this.setState({ 
                     users: searchText === '' ? res.data : res.data.items, 
                     loading: false,  
-                    showClear: true, 
-                    showReset: true
+                    showClear: searchText === '' ? false : true, 
+                    showReset:  searchText === '' ? false :true
                   })
   } 
 
   // show all users
   resetUsers = async () => {
     this.setState({ loading: true });
-    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    const res = await axios.get(`https://api.github.com/users?client_id=${githubClientId}&client_secret=${githubClientSecret}`);
     this.setState({ users: res.data, loading: false,  showClear: false, showReset: false });
   }
 
